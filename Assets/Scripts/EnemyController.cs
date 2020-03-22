f4541915391ff049fd4bbfcdf75c220841a16143
Jordan S <jordan.stoj000@gmail.com>
2020-03-22 17:45:05 +1100

    Added Basic Player Movement

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public int speed;
+    Transform TargetPlayer;
+    Transform Projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TargetPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        Projectile = GameObject.FindGameObjectWithTag("Projectile").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, TargetPlayer.position) > 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, TargetPlayer.position, speed * Time.deltaTime);
+        }
+
+        if(Vector2.Distance(transform.position, Projectile.position) < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    int speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("w"))
+        {
+            transform.localEulerAngles = new Vector3(0, 0, 90);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            transform.localEulerAngles = new Vector3(0, 0, -90);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            transform.localEulerAngles = new Vector3(0, 0, 180);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.localEulerAngles = new Vector3(0, 0, 0);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
